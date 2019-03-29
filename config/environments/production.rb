f8e4ed8b125a53633c28283da7a7e34904917fe9
greatestgoat <ldlwbru0218@gmail.com>
2019-03-30 08:34:59 +0900

    Reset SendGridSettings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,10 +100,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => 'app128081280@heroku.com',
-    :password       => 'whky0ykk7656',
-    # :user_name      => ENV['SENDGRID_USERNAME'],
-    # :password       => ENV['SENDGRID_PASSWORD'],
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
