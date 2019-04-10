47386bc008a2855f9534f41a61ef5dfdc33eb97c
greatestgoat <ldlwbru0218@gmail.com>
2019-04-10 20:56:34 +0900

    Add user microposts secound

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,9 +4,9 @@ if Rails.env.production?
         # Amazon S3用の設定
         :provider              => 'AWS',
         :region                => ENV['ap-northeast-1'],     # 例: 'ap-northeast-1'
-        :aws_access_key_id     => ENV[''],
-        :aws_secret_access_key => ENV['']
+        :aws_access_key_id     => ENV['AKIA6EKM2NW7VQ67ZDDC'],
+        :aws_secret_access_key => ENV['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']
       }
-      config.fog_directory     =  ENV['']
+      config.fog_directory     =  ENV['railstutorialforb']
     end
   end
