8ea9a8f439cec214d1b918079644580fcdde8686
greatestgoat <ldlwbru0218@gmail.com>
2019-04-10 22:59:45 +0900

    AWS S3

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,7 +3,7 @@ if Rails.env.production?
       config.fog_credentials = {
         # Amazon S3用の設定
         :provider              => 'AWS',
-        :region                => ENV['S3_REGION'],     # 例: 'ap-northeast-1'
+        :region                => ENV['S3_REGION'],
         :aws_access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
         :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
       }
